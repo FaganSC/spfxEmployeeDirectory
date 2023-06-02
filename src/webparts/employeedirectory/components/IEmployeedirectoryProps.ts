50e8c384ad9e5597e0523dbdd1f3a50242a4bd55
Shawn Fagan <FaganSC@gmail.com>
2023-06-02 15:55:39 -0400

    Initial Commit

---
+++ src/webparts/employeedirectory/components/IEmployeedirectoryProps.ts
@@ -0,0 +1,7 @@
+export interface IEmployeedirectoryProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/employeedirectory/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IEmployeedirectoryWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'EmployeedirectoryWebPartStrings' {
+  const strings: IEmployeedirectoryWebPartStrings;
+  export = strings;
+}
